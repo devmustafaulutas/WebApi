a7bbb7e845bc150aaa2ed5e2f9236ca6c8062b52
devmustafaulutas <148385039+devmustafaulutas@users.noreply.github.com>
2025-03-06 14:03:34 +0300

    first commit

---
+++ bookDemo/Models/Book.cs
@@ -0,0 +1,9 @@
+namespace bookDemo.Models
+{
+    public class Book
+    {
+        public int Id { get; set;}
+        public string Title { get; set;} = default!;
+        public decimal Price { get; set;}
+    }
+}

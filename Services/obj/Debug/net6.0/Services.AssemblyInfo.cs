2e6ef9919c283b695af1a3f3d0f71d8a49031192
devmustafaulutas <148385039+devmustafaulutas@users.noreply.github.com>
2025-03-12 11:30:17 +0300

    Presentation section

--- Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net6.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cc01524b8b97df765daafd7df9de291619a743f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de8eeedcfe1b560b764fe798dbda5c39a463068")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
